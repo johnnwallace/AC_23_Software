26d9595a858dddbdb381c104dcf1c474a4b26a26
John Wallace <jw0535@princeton.edu>
2023-02-27 18:31:40 -0500

    Added switch block to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
 
+enum FlightState {
+    IDLE1,  // pre-launch idle
+    CALIB,  // calibration
+    COUNT,  // countdown
+    POWER,  // powered flight
+    UNPWR,  // unpowered flight
+    DESC,   // descent
+    LAND,   // landed
+    IDLE2   // post-flight idle
+};
+
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    enum FlightState state = IDLE1;
+
+    switch (state) {
+        case IDLE1:
+            break;
+        case CALIB:
+            break;
+        case COUNT:
+            break;
+        case POWER:
+            break;
+        case UNPWR:
+            break;
+        case DESC:
+            break;
+        case LAND:
+            break;
+        case IDLE2:
+            break;
+    }
+
 }
